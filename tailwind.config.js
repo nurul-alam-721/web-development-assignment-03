4dcd806c63e1feed3e7cc787ca523611443635cb
nurul-alam-721 <nurulalam0404@gmail.com>
2025-01-30 21:43:29 +0600

    navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+   theme: {
+    extend: {
+      colors: {
+        // Define your custom color here
+        primary-color: "#"
+      },
+    },
+  },
+    plugins: [],
+  }
+  
