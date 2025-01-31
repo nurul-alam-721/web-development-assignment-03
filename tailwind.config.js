8422034a3869b871d6a4859ec948be5f7e701496
nurul-alam-721 <nurulalam0404@gmail.com>
2025-01-31 13:09:46 +0600

    changes some styles and mad responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
-   theme: {
-    extend: {
-      colors: {
-        // Define your custom color here
-        primary-color: "#"
-      },
+    theme: {
+      extend: {},
     },
-  },
     plugins: [],
   }
   
